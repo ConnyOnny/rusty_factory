11d688330952962f118c6b99b44c216216b9663c
Constantin Berhard <git.mail.enormator@xoxy.net>
2017-01-16 13:29:01 +0100

    remove shape_factory

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ mod shape;
 mod rectangle;
 mod circle;
 mod triangle;
-mod shape_factory;
-
-use shape_factory::ShapeFactory;
 
 fn main() {
-    let shape_factory = ShapeFactory::new();
-    let shape1_option = shape_factory.get_shape("CIRCLE");
+    let shape1_option = shape::get_shape_by_name("CIRCLE");
     match shape1_option {
         Some(shape1) => {
             // shape1 has type Box<Shape>
--- src/shape.rs
+++ src/shape.rs
@@ -1,3 +1,17 @@
 pub trait Shape {
     fn draw(&self);
 }
+
+use circle::Circle;
+use rectangle::Rectangle;
+use triangle::Triangle;
+
+pub fn get_shape_by_name(shape_type: &str) -> Option<Box<Shape>> {
+    let shape_type_uppercase = shape_type.to_uppercase();
+    match shape_type_uppercase.as_ref() {
+        "CIRCLE" => Some(Box::new(Circle)),
+        "RECTANGLE" => Some(Box::new(Rectangle)),
+        "TRIANGLE" => Some(Box::new(Triangle)),
+        _ => None,
+    }
+}
